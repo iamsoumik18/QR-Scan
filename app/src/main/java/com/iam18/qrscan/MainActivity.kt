156818c98400b1ab1bb3689a189bfdefad45730c
Soumik Saha <iamsoumik18@gmail.com>
2021-06-27 18:01:10 +0530

    Added scan option

--- app/src/main/java/com/iam18/qrscan/MainActivity.kt
+++ app/src/main/java/com/iam18/qrscan/MainActivity.kt
@@ -1,11 +1,116 @@
 package com.iam18.qrscan
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.view.View
+import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
+import com.iam18.qrscan.databinding.ActivityMainBinding
+import com.journeyapps.barcodescanner.CaptureActivity
+import org.json.JSONException
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks, EasyPermissions.RationaleCallbacks {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        EasyPermissions.requestPermissions(
+            this,
+            "This app needs access to your camera so you can take pictures.",
+            123,
+            android.Manifest.permission.CAMERA)
+
+        binding.cardView2.visibility = View.VISIBLE
+
+        binding.btnScan.setOnClickListener {
+            binding.cardView2.visibility = View.VISIBLE
+            binding.cardView1.visibility = View.GONE
+            cameraTask()
+        }
+
+    }
+
+    private fun hasCameraAccess(): Boolean {
+        return EasyPermissions.hasPermissions(this, android.Manifest.permission.CAMERA)
+    }
+
+    private fun cameraTask() {
+
+        if (hasCameraAccess()) {
+
+            var qrScanner = IntentIntegrator(this)
+            qrScanner.setPrompt("scan a QR code")
+            qrScanner.setCameraId(0)
+            qrScanner.setOrientationLocked(false)
+            qrScanner.setBeepEnabled(true)
+            qrScanner.captureActivity = CaptureActivity::class.java
+            qrScanner.initiateScan()
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "This app needs access to your camera so you can take pictures.",
+                123,
+                android.Manifest.permission.CAMERA
+            )
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+
+        var result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_SHORT).show()
+                binding.edtCode.text = ""
+            } else {
+                try {
+                    binding.cardView1.visibility = View.VISIBLE
+                    binding.cardView2.visibility = View.GONE
+                    binding.edtCode.text = result.contents.toString()
+                    binding.edtCode.movementMethod = ScrollingMovementMethod.getInstance()
+                } catch (exception: JSONException) {
+                    Toast.makeText(this, exception.localizedMessage, Toast.LENGTH_SHORT).show()
+                    binding.edtCode.text = ""
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+
+        if (requestCode == AppSettingsDialog.DEFAULT_SETTINGS_REQ_CODE) {
+            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        }
+    }
+
+    override fun onRationaleAccepted(requestCode: Int) {
+
+    }
+
+    override fun onRationaleDenied(requestCode: Int) {
+
     }
 }
